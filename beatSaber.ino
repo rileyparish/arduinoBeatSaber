a35e1c62abaae6cb47c2e0a18046cb713b99bb71
Riley Parish <parish.riley@gmail.com>
2021-11-26 13:50:29 -0700

    Boilerplate.

---
+++ beatSaber.ino
@@ -0,0 +1,15 @@
+/*
+Hey there! This code was originally written for our YouTube channel, https://www.youtube.com/RKadeGaming
+We're glad that you've taken an interest in our project, and we hope that you have a good time building it!
+We've made this code public, and you're free to use it however you like. If you end up sharing the project with others though, 
+we'd appreciate some attribution so that people know where to find more stuff like this.
+Thanks, and have fun! :)
+*/
+
+void setup(){
+
+}
+
+void loop(){
+
+}
