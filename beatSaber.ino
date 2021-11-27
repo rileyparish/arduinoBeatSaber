b125d9d13af3aadfa31aa2ec4eb923f82d3ae76c
Riley Parish <parish.riley@gmail.com>
2021-11-26 17:35:18 -0700

    Initial logic backbone. Haven't tested yet, and need to implement the mouse movements yet.

--- beatSaber.ino
+++ beatSaber.ino
@@ -6,10 +6,103 @@ we'd appreciate some attribution so that people know where to find more stuff li
 Thanks, and have fun! :)
 */
 
-void setup(){
+// this sketch is intended for the right-hand control
 
+const int upperLeft = A0;
+const int upperRight = A1;
+const int lowerLeft = A2; 
+const int lowerRight = A3;
+
+const int lightThreshold = 300;     // when a resistor gets a reading that is smaller than this value, we want to record it (resistance decreases with light, I think)
+const int activeDiff = 50;          // if the resistors were active within this time frame, then I want to register a swipe
+const int swipeCooldown = 500;      // how long to wait between swipe commands (to prevent double-presses)
+
+unsigned long upperLeftLastActive = millis();
+unsigned long upperRightLastActive = millis();
+unsigned long lowerLeftLastActive = millis();
+unsigned long lowerRightLastActive = millis();
+
+// the time that the last swipe was sent
+unsigned long latestSwipe = millis();
+
+void setup(){
+    pinMode(upperLeft, INPUT);
+    pinMode(upperRight, INPUT);
+    pinMode(lowerLeft, INPUT);
+    pinMode(lowerRight, INPUT);
 }
 
 void loop(){
+    updateResistorStates();
+    executeMovement();
+}
+
+// based on the last time the photoresistors were active, determine if a 
+void executeMovement(){
+    // if it's been long enough since the last movement, evaluate for new movements
+    if(millis() - latestSwipe < swipeCooldown){
+        evaluateLeftSwipe();
+        evaluateRightSwipe();
+        evaluateUpSwipe();
+        evaluateDownSwipe();
+    }
+}
+
+// read from all the resistors and update the history based on the readings
+void updateResistorStates(){
+    if(analogRead(upperLeft) < lightThreshold){
+        upperLeftLastActive = millis();
+    }
+    if(analogRead(upperRight) < lightThreshold){
+        upperRightLastActive = millis();
+    }
+    if(analogRead(lowerLeft) < lightThreshold){
+        lowerLeftLastActive = millis();
+    }
+    if(analogRead(lowerRight) < lightThreshold){
+        lowerRightLastActive = millis();
+    }
+}
+
+// based on the resistor histories, should a left swipe be acivated?
+bool evaluateLeftSwipe(){
+    if(upperLeftLastActive - upperRightLastActive < activeDiff || lowerLeftLastActive - lowerRightLastActive < activeDiff){
+        swipeLeft();
+    }
+    latestSwipe = millis();
+}
+bool evaluateRightSwipe(){
+    // if the resistors have been activated within the threshold, 
+    if(upperRightLastActive - upperLeftLastActive < activeDiff || lowerRightLastActive - lowerLeftLastActive < activeDiff){
+        swipeRight();
+    }
+    latestSwipe = millis();
+}
+bool evaluateUpSwipe(){
+    if(upperLeftLastActive - lowerLeftLastActive < activeDiff || upperRightLastActive - lowerRightLastActive < activeDiff){
+        swipeUp();
+    }
+    latestSwipe = millis();
+}
+bool evaluateDownSwipe(){
+    if(lowerLeftLastActive - upperLeftLastActive < activeDiff || lowerRightLastActive - upperRightLastActive < activeDiff){
+        swipeDown();
+    }
+    latestSwipe = millis();
+}
+
+void swipeLeft(){
+
+}
+
+void swipeRight(){
+
+}
+
+void swipeUp(){
+
+}
+
+void swipeDown(){
 
 }
